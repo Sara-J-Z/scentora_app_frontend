2122c900e8cd9d6eb20f9762d1d63f2eece0486c
Sara <sara-jassim@hotmail.com>
2025-08-02 13:47:21 +0300

    setup frontend

---
+++ scentora-frontend/src/components/UserList.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import api from '../api';
+function UserList() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    api.get('/users/')
+      .then(res => setUsers(res.data))
+      .catch(err => console.error(err));
+  }, []);
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default UserList;
